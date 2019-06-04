23b005bbef74148a020042b4f76280563a108f91
gifmr <usr0102269@gmo.local>
2019-06-04 16:41:45 +0900

    add cpp template for contests

---
+++ template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define REP(i, n) for (int i = (int)(0); i < (int)(n); ++i)
+#define REPS(i, n) for (int i = (int)(1); i <= (int)(n); ++i)
+#define RREP(i, n) for (int i = ((int)(n)-1); i >= 0; i--)
+#define RREPS(i, n) for (int i = ((int)(n)); i > 0; i--)
+#define IREP(i, m, n) for (int i = (int)(m); i < (int)(n); ++i)
+#define IREPS(i, m, n) for (int i = (int)(m); i <= (int)(n); ++i)
+#define FOR(e, c) for (auto &e : c)
+#define SORT(v, n) sort(v, v + n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define RVISORT(v) sort(v.begin(), v.end(), greater<int>());
+#define ALL(v) v.begin(), v.end()
+
+template<class T, class C> void chmax(T& a, C b){ a>b?:a=b; }
+template<class T, class C> void chmin(T& a, C b){ a<b?:a=b; }
+
+int main()
+{
+
+}
