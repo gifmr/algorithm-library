046e59fa800b38548d03a2797099108bc1f89fd7
gifmr <ntkbusy0213@gmail.com>
2019-08-10 01:20:18 +0900

    add gcd, lcm

--- template.cpp
+++ template.cpp
@@ -103,6 +103,19 @@ map<ll, ll> primeFact(ll n) {
   return res;
 }
 
+int gcd(int a, int b){
+  if (a < b) {
+      a ^= b;
+      b ^= a;
+      a ^= b;
+  }
+  return b ? gcd(b, a % b) : a;
+}
+
+int lcm(int a, int b){
+  return a * b / gcd(a, b);
+}
+
 void printv(vi& v){
   rep(i,v.size()) cout << v[i] << " ";
   cout << endl;
