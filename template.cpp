9c322a1f658b1c52747d1af98d7e5d090063f353
gifmr <ntkbusy0213@gmail.com>
2019-09-29 19:42:07 +0900

    update template

--- template.cpp
+++ template.cpp
@@ -13,9 +13,11 @@ using namespace std;
 #define rvisort(v) sort(v.begin(), v.end(), greater<int>());
 #define all(v) v.begin(), v.end()
 #define mp(n, m) make_pair(n, m);
+#define cout(d) cout<<d<<endl;
 #define coutd(d) cout<<std::setprecision(10)<<d<<endl;
 #define cinline(n) getline(cin,n);
 #define replace_all(s, b, a) replace(s.begin(),s.end(), b, a);
+#define int long long
 
 using ll = long long;
 using vi = vector<int>;
@@ -35,96 +37,8 @@ template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; }
 int GCD(int a, int b) { return b ? GCD(b, a%b) : a; }
 int LCM(int a, int b) { return a * b / GCD(a, b); }
 
-struct mint {
-  ll x;
-  mint(ll x=0):x(x%MOD){}
-  mint& operator+=(const mint a) {
-    if ((x += a.x) >= MOD) x -= MOD;
-    return *this;
-  }
-  mint& operator-=(const mint a) {
-    if ((x += MOD-a.x) >= MOD) x -= MOD;
-    return *this;
-  }
-  mint& operator*=(const mint a) {
-    (x *= a.x) %= MOD;
-    return *this;
-  }
-  mint operator+(const mint a) const {
-    mint res(*this);
-    return res+=a;
-  }
-  mint operator-(const mint a) const {
-    mint res(*this);
-    return res-=a;
-  }
-  mint operator*(const mint a) const {
-    mint res(*this);
-    return res*=a;
-  }
-};
-
-mint c[4005][4005];
-void init() {
-  c[0][0] = 1;
-  for (int i = 0; i <= 4000; i++) {
-    for (int j = 0; j <= i; j++) {
-      c[i+1][j] += c[i][j];
-      c[i+1][j+1] += c[i][j];
-    }
-  }
-}
-mint comb(int n, int k) {
-  return c[n][k];
-}
-
-bool IsPrime(int num)
+signed main()
 {
-    if (num < 2) return false;
-    else if (num == 2) return true;
-    else if (num % 2 == 0) return false;
-
-    double sqrtNum = sqrt(num);
-    for (int i = 3; i <= sqrtNum; i += 2)
-    {
-        if (num % i == 0)
-        {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-map<ll, ll> primeFact(ll n) {
-  map<ll, ll> res;
-  for (ll i = 2; i * i <= n; i++) {
-    while (n % i == 0) {
-      res[i]++;
-      n /= i;
-    }
-  }
-  if (n != 1) res[n]++;
-  return res;
-}
-
-void printv(vi& v){
-  rep(i,v.size()) cout << v[i] << " ";
-  cout << endl;
-}
-void printv(vll& v){
-  rep(i,v.size()) cout << v[i] << " ";
-  cout << endl;
-}
-int vtotal(vi& v){
-  int total=0;
-  rep(i,v.size()) total+=v[i];
-  return total;
-}
-
-int main()
-{
-  //init(); // mint初期化
   cin.tie( 0 );
   ios::sync_with_stdio( false );
 
