21ad01823eadf737df0e3146669addd8efaf8d46
gifmr <ntkbusy0213@gmail.com>
2019-09-29 19:40:49 +0900

    format

--- library/math/eratosthenes.cpp
+++ library/math/eratosthenes.cpp
@@ -5,25 +5,25 @@ using namespace std;
 // 参考: https://mathtrain.jp/eratosthenes
 // O(nloglogn)
 
-const int MAXN = 1000005;   // 素数を探す範囲
-bool _is_prime[ MAXN ];     // 添え字に対応する値の素数判定を格納
-vector<long long> P;        // 素数を格納する
+const int MAXN = 1000005; // 素数を探す範囲
+bool _is_prime[MAXN];     // 添え字に対応する値の素数判定を格納
+vector<long long> P;      // 素数を格納する
 
-void eratosthenes( const long long N )
+void eratosthenes(const long long N)
 {
-    for( long long i = 0; i <= N; i++ )
+  for (long long i = 0; i <= N; i++)
+  {
+    _is_prime[i] = true; // trueで初期化.
+  }
+  for (long long i = 2; i * i <= N; i++)
+  {
+    if (_is_prime[i])
     {
-        _is_prime[ i ] = true;                            // trueで初期化.
-    }    
-    for( long long i = 2; i*i <= N; i++ )
-    {
-        if( _is_prime[ i ] )
-        {
-            for( long long j = 2 * i; j <= N; j += i )
-            {
-                _is_prime[ j ] = false;                   // iの倍数を篩にかける
-            }
-            P.emplace_back( i );
-        }
+      for (long long j = 2 * i; j <= N; j += i)
+      {
+        _is_prime[j] = false; // iの倍数を篩にかける
+      }
+      P.emplace_back(i);
     }
+  }
 }
