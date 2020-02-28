705c28abf6c6b2952d8fbe4eb9f9e5813f823fe3
gifmr <ntkbusy0213@gmail.com>
2020-02-29 05:04:10 +0900

    update

--- library/data_structure/union_find_tree_v2.cpp
+++ library/data_structure/union_find_tree_v2.cpp
@@ -7,10 +7,10 @@ using namespace std;
 
 class UnionFind{
   public:
-  // 親の番号を格納する．親の場合は-(その集合のサイズ)
+  // 親番号を格納する．グループサイズを取得可能にするため、親の場合は-(その集合のサイズ)を格納する鵜
   vector<int> Parent; 
   
-  // 全て-1
+  // 初期化時は全て-1
   UnionFind(int N){
     Parent = vector<int>(N, -1);
   }
@@ -40,11 +40,16 @@ class UnionFind{
     // 大小が逆ならひっくり返す
     if(size(A) < size(B)) swap(A, B);
     
-    // Aのサイズを更新する
+    // 併合に伴いグループのサイズを更新する. Aのサイズを更新する
     Parent[A] += Parent[B];
     // Bの親をAに変更する
     Parent[B] = A;
     
     return true;
+  }
+  
+  // AとBが同じグループに属するかを判定
+  bool isSame(int A, int B){
+    return root(A) == root(B);
   }
 };
