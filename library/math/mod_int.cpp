65e268ae83c1c554dd4cff4f26f8b181c1727aac
gifmr <ntkbusy0213@gmail.com>
2020-03-11 20:58:39 +0900

    update

--- library/math/mod_int.cpp
+++ library/math/mod_int.cpp
@@ -1,50 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MOD = 1e9+7;
-
-struct mint {
-  long long x;
-  mint(long long x=0):x(x%MOD){}
-  mint& operator+=(const mint a) {
-    if ((x += a.x) >= MOD) x -= MOD;
-    return *this;
-  }
-  mint& operator-=(const mint a) {
-    if ((x += MOD-a.x) >= MOD) x -= MOD;
-    return *this;
-  }
-  mint& operator*=(const mint a) {
-    (x *= a.x) %= MOD;
-    return *this;
-  }
-  mint operator+(const mint a) const {
-    mint res(*this);
-    return res+=a;
-  }
-  mint operator-(const mint a) const {
-    mint res(*this);
-    return res-=a;
-  }
-  mint operator*(const mint a) const {
-    mint res(*this);
-    return res*=a;
-  }
-};
-
-mint c[4005][4005];
-
-// 本ライブラリは実行前に初期化が必要
-void init() {
-  c[0][0] = 1;
-  for (int i = 0; i <= 4000; i++) {
-    for (int j = 0; j <= i; j++) {
-      c[i+1][j] += c[i][j];
-      c[i+1][j+1] += c[i][j];
+template<int MOD> struct Fp {
+    long long val;
+    constexpr Fp(long long v = 0) noexcept : val(v % MOD) {
+        if (val < 0) v += MOD;
     }
-  }
-}
-
-mint comb(int n, int k) {
-  return c[n][k];
-}
+    constexpr int getmod() { return MOD; }
+    constexpr Fp operator - () const noexcept {
+        return val ? MOD - val : 0;
+    }
+    constexpr Fp operator + (const Fp& r) const noexcept { return Fp(*this) += r; }
+    constexpr Fp operator - (const Fp& r) const noexcept { return Fp(*this) -= r; }
+    constexpr Fp operator * (const Fp& r) const noexcept { return Fp(*this) *= r; }
+    constexpr Fp operator / (const Fp& r) const noexcept { return Fp(*this) /= r; }
+    constexpr Fp& operator += (const Fp& r) noexcept {
+        val += r.val;
+        if (val >= MOD) val -= MOD;
+        return *this;
+    }
+    constexpr Fp& operator -= (const Fp& r) noexcept {
+        val -= r.val;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+    constexpr Fp& operator *= (const Fp& r) noexcept {
+        val = val * r.val % MOD;
+        return *this;
+    }
+    constexpr Fp& operator /= (const Fp& r) noexcept {
+        long long a = r.val, b = MOD, u = 1, v = 0;
+        while (b) {
+            long long t = a / b;
+            a -= t * b; swap(a, b);
+            u -= t * v; swap(u, v);
+        }
+        val = val * u % MOD;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+    constexpr bool operator == (const Fp& r) const noexcept {
+        return this->val == r.val;
+    }
+    constexpr bool operator != (const Fp& r) const noexcept {
+        return this->val != r.val;
+    }
+    friend constexpr ostream& operator << (ostream &os, const Fp<MOD>& x) noexcept {
+        return os << x.val;
+    }
+    friend constexpr istream& operator >> (istream &is, Fp<MOD>& x) noexcept {
+        return is >> x.val;
+    }
+    friend constexpr Fp<MOD> modpow(const Fp<MOD> &a, long long n) noexcept {
+        if (n == 0) return 1;
+        auto t = modpow(a, n / 2);
+        t = t * t;
+        if (n & 1) t = t * a;
+        return t;
+    }
+};
