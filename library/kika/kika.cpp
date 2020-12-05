dadb8de9507011c5233cee58df42a6e8d9abf86d
gifmr <ntkbusy0213@gmail.com>
2020-12-05 20:15:59 +0900

    add

---
+++ library/kika/kika.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+// 線分交差判定
+bool f(ll ax, ll ay, ll bx, ll by, ll cx, ll cy, ll dx, ll dy) {
+  ll ta = (cx - dx) * (ay - cy) + (cy - dy) * (cx - ax);
+  ll tb = (cx - dx) * (by - cy) + (cy - dy) * (cx - bx);
+  ll tc = (ax - bx) * (cy - ay) + (ay - by) * (ax - cx);
+  ll td = (ax - bx) * (dy - ay) + (ay - by) * (ax - dx);
+
+  return tc * td < 0 && ta * tb < 0;
+  // return tc * td <= 0 && ta * tb <= 0; // 端点を含む場合
+};
