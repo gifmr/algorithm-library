ee3f5b8a2a1ebe6418203e69db877656b91664d4
gifmr <ntkbusy0213@gmail.com>
2021-02-21 00:25:27 +0900

    update

--- library/math/prime_tool.cpp
+++ library/math/prime_tool.cpp
@@ -26,23 +26,10 @@ bool quick_is_prime(int n) {
   return true;
 }
 
-/// 約数列挙 O(√n)
-vector<int> divisor(int n) {
-  vector<int> res;
-  for (int i = 1; i * i <= n; i++) {
-    if (n % i == 0) {
-      res.push_back(i);
-      if (i != n / i)
-        res.push_back(n / i);
-    }
-  }
-  return res;
-}
-
 /// 素因数分解 O(√n)
-map<int, int> prime_factor(int n) {
+map<int, int> prime_factor(long long n) {
   map<int, int> res;
-  for (int i = 2; i * i <= n; i++) {
+  for (long long i = 2; i * i <= n; i++) {
     while (n % i == 0) {
       ++res[i];
       n /= i;
@@ -51,18 +38,4 @@ map<int, int> prime_factor(int n) {
   if (n != 1)
     res[n] = 1;
   return res;
-}
-
-/// 素因数の個数数え上げ
-long long cnt_prime_factor(long long N) {
-  long long res = 0;
-  for (long long i = 2; i * i <= N; i++) {
-    if (N % i == 0) {
-      res++;
-      while (N % i == 0)
-        N /= i;
-    }
-  }
-  if (N != 1)
-    res++;
 }
