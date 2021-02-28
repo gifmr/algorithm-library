9bf10d806e2c0435c1005742ed18fb3e44a03aed
gifmr <ntkbusy0213@gmail.com>
2021-02-28 09:12:57 +0900

    tweak

--- library/flow/primal_dual.cpp
+++ library/flow/primal_dual.cpp
@@ -7,8 +7,7 @@ private:
     int to, rev;
     capacity_t cap;
     cost_t cost;
-    Edge(int to, int rev, capacity_t cap, cost_t cost)
-        : to(to), rev(rev), cap(cap), cost(cost) {}
+    Edge(int to, int rev, capacity_t cap, cost_t cost) : to(to), rev(rev), cap(cap), cost(cost) {}
   };
   vector<vector<Edge>> G;
   vector<int> prev_v, prev_e;
@@ -32,21 +31,17 @@ public:
     while (flow > 0) {
       vector<cost_t> dist(n, INF);
       dist[s] = 0;
-      priority_queue<pair<cost_t, int>, vector<pair<cost_t, int>>,
-                     greater<pair<cost_t, int>>>
-          que;
+      priority_queue<pair<cost_t, int>, vector<pair<cost_t, int>>, greater<pair<cost_t, int>>> que;
       que.emplace(0, s);
       while (!que.empty()) {
         cost_t c;
         int v;
         tie(c, v) = que.top();
         que.pop();
-        if (c > dist[v])
-          continue;
+        if (c > dist[v]) continue;
         for (int i = 0; i < G[v].size(); i++) {
           auto &edge = G[v][i];
-          if (edge.cap > 0 and
-              dist[edge.to] > dist[v] + edge.cost + h[v] - h[edge.to]) {
+          if (edge.cap > 0 and dist[edge.to] > dist[v] + edge.cost + h[v] - h[edge.to]) {
             dist[edge.to] = dist[v] + edge.cost + h[v] - h[edge.to];
             prev_v[edge.to] = v;
             prev_e[edge.to] = i;
@@ -54,10 +49,8 @@ public:
           }
         }
       }
-      if (dist[t] == INF)
-        return -1;
-      for (int v = 0; v < n; v++)
-        h[v] += dist[v];
+      if (dist[t] == INF) return -1;
+      for (int v = 0; v < n; v++) h[v] += dist[v];
       capacity_t d = flow;
       for (int v = t; v != s; v = prev_v[v]) {
         d = min(d, G[prev_v[v]][prev_e[v]].cap);
