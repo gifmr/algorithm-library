3afbf92ec275ee46ef714cfe5d881fc066c11b25
gifmr <ntkbusy0213@gmail.com>
2021-03-02 03:41:53 +0900

    refactor

--- library/flow/ford_fulkerson.cpp
+++ library/flow/ford_fulkerson.cpp
@@ -1,42 +1,29 @@
-template <class T>
-struct Edge {
-  int to, from, rev;
-  T cap;
-  Edge(int from, int to, int rev, T cap)
-      : to(to), from(from), rev(rev), cap(cap) {}
-};
-
-template <class T>
-struct Graph {
-  vector<vector<Edge<T>>> G;
-  vector<Edge<T>> &operator[](int i) { return G[i]; }
-  const size_t size() const { return G.size(); }
-  Graph(int n) : G(n) {}
-  void add_edge(int from, int to, T cap) {
-    G[from].push_back(Edge<T>(from, to, (int)G[to].size(), cap));
-    G[to].push_back(Edge<T>(to, from, (int)G[from].size() - 1, 0));
-  }
-  Edge<T> &get_rev(Edge<T> &edge) { return G[edge.to][edge.rev]; }
-};
-
 /// @see[https://pione.hatenablog.com/entry/2021/02/26/003115]
 template <class T>
 class FordFulkerson {
-  const T INF = 1e9;
-
 private:
+  const T INF = 1e9;
+  
+  struct Edge {
+    int to, from, rev; T cap;
+    Edge(int from, int to, int rev, T cap) : to(to), from(from), rev(rev), cap(cap) {}
+  };
+  
+  vector<vector<Edge>> G;
+  
+  Edge &get_rev(Edge &edge) { return G[edge.to][edge.rev]; }
+  
   vector<bool> used;
-  T dfs(Graph<T> &G, int v, int t, T flow) {
-    if (v == t)
-      return flow;
+  
+  T dfs(int v, int t, T flow) {
+    if (v == t) return flow;
     used[v] = true;
     for (auto &e : G[v]) {
-      if (used[e.to] or e.cap <= 0)
-        continue;
-      T d = dfs(G, e.to, t, min(flow, e.cap));
+      if (used[e.to] or e.cap <= 0) continue;
+      T d = dfs(e.to, t, min(flow, e.cap));
       if (d > 0) {
         e.cap -= d;
-        G.get_rev(e).cap += d;
+        get_rev(e).cap += d;
         return d;
       }
     }
@@ -44,13 +31,19 @@ private:
   }
 
 public:
-  FordFulkerson() {}
-  T max_flow(Graph<T> &G, int s, int t) {
+  FordFulkerson(int n) { G.resize(n); }
+  
+  void add_edge(int from, int to, T cap) {
+    G[from].push_back(Edge(from, to, (int)G[to].size(), cap));
+    G[to].push_back(Edge(to, from, (int)G[from].size() - 1, 0));
+  }
+  
+  T get_max_flow(int s, int t) {
     int n = G.size();
     T res = 0;
     while (true) {
       used.assign(n, false);
-      T flow = dfs(G, s, t, INF);
+      T flow = dfs(s, t, INF);
       if (flow > 0) {
         res += flow;
       } else {
